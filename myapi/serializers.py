d1b315e030016f2c239e06d24e2edd2cbba727a5
crazycodeop <85179778+crazycodeop@users.noreply.github.com>
2023-01-21 12:21:06 +0530

    Update serializers.py

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class EntriesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Entries
-        fields=('id', 'Date_Posted', ' Proptype','Link', 'Owner', 'bHK', 'Locality', 'city', 'Price', 'Carpet_Area', 'Furnishing', 'Bathrooms', 'Facing', 'Status', 'Transaction', 'Price_Sqft', 'Floor', 'Description')
+        fields=('id', 'Date_Posted', 'Link', 'Owner', 'bHK', 'Locality', 'city', 'Price', 'Carpet_Area', 'Furnishing', 'Bathrooms', 'Facing', 'Status', 'Transaction', 'Price_Sqft', 'Floor', 'Description')
