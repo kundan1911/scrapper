d6f0b42a14c46b1b048d78b912859484137650c3
kundan1911 <83800542+kundan1911@users.noreply.github.com>
2023-01-25 23:30:31 +0530

    Update urls.py

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include,path
 from rest_framework import routers
-from . import views
+from . import views 
 
-
+# routing the database to the respective url
 router = routers.DefaultRouter()
-router.register(r'Entries', views.EntryViewSet)
-
+router.register(r'res_sale_model', views.resSaleViewSet)
+router.register(r'res_rent_model', views.resRentViewSet)
+router.register(r'res_pg_model', views.resPgViewSet)
+router.register(r'comm_sale_model', views.commSaleViewSet)
+router.register(r'comm_lease_model', views.commLeaseViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
