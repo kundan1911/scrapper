73f7b267dcd7fb6bf967760de4b8ef28e71cfadf
kundan1911 <83800542+kundan1911@users.noreply.github.com>
2023-01-25 23:37:23 +0530

    Update settings.py

--- scrapper_site/settings.py
+++ scrapper_site/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
-    'myapi.apps.MyapiConfig',
+    'myapinew',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
